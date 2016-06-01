2d95205eee81c474a5258ad9e662912923734466
Radium Zheng <ztl8702@msn.com>
2016-06-01 15:13:11 +1000

    added new classes for HTML import

---
+++ src/Asset/HTMLCollectionGrouper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ztl8702
+ * Date: 1/06/16
+ * Time: 2:51 PM
+ */
+
+namespace Drupal\twig_polymer\Asset;
+
+
+use Drupal\Core\Asset\AssetCollectionGrouperInterface;
+
+class HTMLCollectionGrouper implements AssetCollectionGrouperInterface
+{
+
+}
