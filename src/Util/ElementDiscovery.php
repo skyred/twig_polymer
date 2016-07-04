3209e6f748d768c334d2085d6770c7aa5af3fe42
Radium Zheng <ztl8702@msn.com>
2016-07-04 12:29:02 +1000

    ElementDiscovery by path

--- src/Util/ElementDiscovery.php
+++ src/Util/ElementDiscovery.php
@@ -6,32 +6,74 @@
 
 namespace Drupal\twig_polymer\Util;
 
+use Drupal\Core\Theme\ThemeInitializationInterface;
+
 /**
  * Element Discovery Service
  */
 class ElementDiscovery {
-  public function __construct() {
+  protected $root;
 
+  /**
+   * ElementDiscovery constructor.
+   *
+   * @param string $root
+   *   The app root.
+   * @param \Drupal\Core\Theme\ThemeInitializationInterface $theme_initialization
+   */
+  public function __construct($root, ThemeInitializationInterface $theme_initialization) {
+    $this->root = $root;
+    $this->themeInitialization = $theme_initialization;
   }
 
-  public function getElementInternalPath($filename, $themename = '') {
+  protected function getThemePath($themeName) {
+    return $this->root . drupal_get_path('theme', $themeName);
+  }
+
+  /**
+   * @param $themeName
+   *   The theme name for query.
+   *
+   * @return string|bool
+   *   The parent theme name, or FALSE if not found.
+   */
+  protected function getParentTheme($themeName) {
+    return $this->themeInitialization->getActiveThemeByName($themeName)->getBaseThemes()[0];
+  }
+
+  public function getElementInternalPath($filename, $themename = FALSE) {
 
     // Order of discovery:
-    // 1. Custom element in current theme (/themes/my-theme/my-element)
+    // 1. Custom element in current theme (/themes/my-theme/my-elements)
     // 2. 3rd Party element in current theme (/themes/my-theme/bower_components)
-    // 3. Custom element in parent theme (/themes/base-theme/my-element)
+    // 3. Custom element in parent theme (/themes/base-theme/my-elements)
     // 4. 3rd Party element in parent theme (/themes/base-theme/bower_components)
     // 5. Repeat 3&4 until there is no parent theme
     // 6. Global 3rd party components (/libraries/bower_components)
 
-    if (substr($filename, -5) === ".html") {
-    // if filename ends with ".html" we load the element from vendor library.
-      return base_path() . drupal_get_path('module', 'twig_polymer'). '/' . $this->config->get("path_components") . '/'. $filename;
-    } else {
-      $polymer_url = \Drupal::url("twig_polymer.get_element_current_theme", ["elementname" => $filename]);
-      return $polymer_url;
+    if ($themename === FALSE) {
+      // Global (6)
+      $path = $this->root . '/libraries/bower_components/' . $filename;
+      return (is_file($path)) ? $path : FALSE;
+    }
+    else {
+      // TODO: needs test when drupal is installed in a non-root directory.
+      $themePath = $this->root . drupal_get_path('theme', $themename);
+
+      $customElement = $themePath . '/my-elements/' . $filename);
+      if (is_file($customElement)) {
+        return $customElement;
+      }
+      else {
+        $bowerElement = $themePath . '/my-elements/' . $filename);
+        if (is_file($bowerElement)) {
+          return $bowerElement;
+        }
+        else {
+          return $this->getElementInternalPath($filename, $this->getParentTheme($themename));
+        }
+      }
     }
   }
-
 
 }
