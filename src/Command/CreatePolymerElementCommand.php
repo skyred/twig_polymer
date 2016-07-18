109ca079872248d09c72ad01749752462e26f880
Radium Zheng <ztl8702@msn.com>
2016-07-18 23:59:00 +1000

    drupal console commands

---
+++ src/Command/CreatePolymerElementCommand.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Drupal\twig_polymer\Command;
+
+use Drupal\Component\Uuid\Com;
+use Drupal\twig_polymer\Generator\PolymerElementGenerator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as BaseCommand;
+use Drupal\Console\Generator\CommandGenerator;
+use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
+use Drupal\Console\Command\Shared\ModuleTrait;
+use Drupal\Console\Command\Shared\ConfirmationTrait;
+use Drupal\Console\Style\DrupalStyle;
+
+/**
+ * Class CreatePolymerElementCommand.
+ *
+ * @package Drupal\twig_polymer
+ */
+class CreatePolymerElementCommand extends GeneratorCommand {
+
+  use ContainerAwareCommandTrait;
+  use ModuleTrait;
+  use ConfirmationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('polymer:element')
+      ->setDescription($this->trans('commands.polymer.element.description'))
+      ->addOption(
+        'theme',
+        '',
+        InputOption::VALUE_REQUIRED,
+        $this->trans('polymer.element.options.theme')
+      )
+      ->addOption(
+        'package',
+        'my-element',
+        InputOption::VALUE_OPTIONAL,
+        $this->trans('polymer.element.option.package')
+      )
+      ->addOption(
+        'element',
+        'my-element',
+        InputOption::VALUE_REQUIRED,
+        $this->trans('polymer.element.option.element')
+      )
+      ->addOption(
+        'create-style',
+        '',
+        InputOption::VALUE_NONE,
+        $this->trans('polymer.element.option.create-style')
+      );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $io = new DrupalStyle($input, $output);
+
+    $text = sprintf(
+      'I am a new generated command for the module: %s',
+      $this->getModule()
+    );
+
+    $theme = $input->getOption('theme');
+    $yes = $input->hasOption('yes')?$input->getOption('yes'):false;
+    $package = $input->getOption('package');
+    $elementName = $input->getOption('element');
+    $createStyle = $input->getOption('create-style');
+
+    if (!$this->confirmGeneration($io, $yes)) {
+      return;
+    }
+
+    $this
+      ->getGenerator()
+      ->generate($theme, $package, $elementName, $createStyle);
+
+    $io->info($text);
+  }
+  protected function interact(InputInterface $input, OutputInterface $output)
+  {
+    $io = new DrupalStyle($input, $output);
+    // --theme option
+    $theme = $input->getOption('theme');
+    if (!$theme) {
+      // @see Drupal\Console\Command\Shared\ModuleTrait::moduleQuestion
+      $theme = $this->moduleQuestion($output);
+      $input->setOption('theme', $theme);
+    }
+    // --package
+    $package = $input->getOption('package');
+    if (!$package) {
+      $package = $io->ask(
+        $this->trans('polymer.element.options.package'),
+        'my-element'
+      );
+      $input->setOption('package', $package);
+    }
+    // --element option
+    $element = $input->getOption('element');
+    if (!$element) {
+      $element = $io->ask(
+        $this->trans('polymer.element.options.element'),
+        'my-element'
+      );
+      $input->setOption('element', $element);
+    }
+    // --container-aware option
+    $createStyle = $input->getOption('create-style');
+    if (!$createStyle) {
+      $createStyle = $io->confirm(
+        $this->trans('polymer.element.options.create-style'),
+        true
+      );
+      $input->setOption('create-style', $createStyle);
+    }
+  }
+
+  protected function createGenerator() {
+    return new PolymerElementGenerator();
+  }
+
+
+}
---
+++ src/Generator/PolymerElementGenerator.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\Console\Generator\CommandGenerator.
+ */
+namespace Drupal\twig_polymer\Generator;
+
+use Drupal\Contsole\Generator\Generator;
+
+class PolymerElementGenerator extends Generator
+{
+  /**
+   * Generator Plugin Block.
+   *
+   * @param string  $module         Module name
+   * @param string  $name           Command name
+   * @param string  $class          Class name
+   * @param boolean $containerAware Container Aware command
+   */
+  public function generate($theme, $package, $elementName, $createStyle)
+  {
+    $parameters = [
+      'theme' => $theme,
+      'element' => $elementName,
+      'package' => $package,
+      'create_style' => $createStyle,
+      'style_element_name' => $elementName . '-styles',
+    ];
+    $messages['description'] = 'Say hello';
+
+    $this->renderFile(
+      'console/element.html.twig',
+      $this->getSite()->getThemePath($theme).'my-elements/'.$package.'/'.$elementName.'.html',
+      $parameters
+    );
+
+    if ($createStyle) {
+      $this->renderFile(
+        'console/style.html.twig',
+        $this->getSite()->getThemePath($theme).'my-elements/'.$package.'/'.$elementName.'-styles.html',
+        $parameters
+      );
+    }
+  }
+}
